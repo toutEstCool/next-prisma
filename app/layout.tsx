19910b565e260963801b831441e982938ca2e941
Danil Panarin <danilpanarin@thefrontends-MacBook-Air.local>
2023-05-29 20:14:52 +0600

    [PM-#864et4jkn]: initial project

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Next 13 Prisma ORM',
+  description: 'My personal project',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
